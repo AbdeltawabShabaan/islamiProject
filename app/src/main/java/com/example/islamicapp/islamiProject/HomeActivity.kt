f9e7333ecdc23af10e513593fbd6e512004a1cce
Abdeltawab <abdeltawabshabaan078@gmail.com>
2023-10-28 21:17:11 +0200

    first commit1

---
+++ app/src/main/java/com/example/islamicapp/islamiProject/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.islamicapp.islamiProject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.islamicapp.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
--- app/src/main/java/com/example/islamicapp/islamiProject/SplashActivity.kt
+++ app/src/main/java/com/example/islamicapp/islamiProject/SplashActivity.kt
@@ -1,12 +1,23 @@
 package com.example.islamicapp.islamiProject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.islamicapp.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        Handler().postDelayed(object : Runnable {
+            override fun run() {
+                val intent = Intent(this@SplashActivity,HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+        },2500)
+
     }
 }
