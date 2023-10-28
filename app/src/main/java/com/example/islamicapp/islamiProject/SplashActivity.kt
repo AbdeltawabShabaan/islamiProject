12e2964ee07f9eed190041b6bd95c1777f751f7b
Abdeltawab <abdeltawabshabaan078@gmail.com>
2023-10-28 20:40:34 +0200

    first commit

---
+++ app/src/main/java/com/example/islamicapp/islamiProject/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.islamicapp.islamiProject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.islamicapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
